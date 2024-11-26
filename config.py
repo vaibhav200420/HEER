a5f69f1e649818c8a2800fc3edf4ace994f2b634
Vaibhav Gupta <128729218+vaibhav200420@users.noreply.github.com>
2024-11-26 12:18:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,12 +9,12 @@ from pyrogram import filters
 load_dotenv()
 # ------------------------------------
 # -----------------------------------------------------
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "20881635"))
+API_HASH = getenv("API_HASH", "5f66e88a2b133c1f77f4b7dd6807b8ce")
 # ------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN", "8056822116:AAG94Et7TN9A86Z3KeTsqAjPAk_8L8UZcJ4")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","")
+OWNER_USERNAME = getenv("OWNER_USERNAME","II_HaRsH_YaDv_II")
 # --------------------------------------------------------
 BOT_USERNAME = getenv("BOT_USERNAME" , "InsaneMusic_Bot")
 # --------------------------------------------------------
@@ -26,7 +26,7 @@ ASSUSERNAME = getenv("ASSUSERNAME")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://vg766337:xksmuYTJEN6nT9j2@yukkimusic-moded-repo.ianrw.mongodb.net/?retryWrites=true&w=majority&appName=YukkiMusic-Moded-Repo")
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 
@@ -35,10 +35,10 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 # ----------------------------------------------------------------
 
 # ----------------------------------------------------------------
-LOGGER_ID = int(getenv("LOGGER_ID", -1002141133985))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002108873378))
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
-OWNER_ID = int(getenv("OWNER_ID", 7147100550))
+OWNER_ID = int(getenv("OWNER_ID", "1750583099"))
 # -----------------------------------------------------------------
 # -----------------------------------------------------------------
 
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FONT_CHANNEL_01")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Exampurrs")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Ps_Corporation_Com")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", https://t.me/+sW_L1vwSZ1Y4NDQ9")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
@@ -114,8 +114,8 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "5242880000"))
 # ------------------------------------
 # ------------------------------------
 # ------------------------------------
-STRING1 = getenv("STRING_SESSION", None)
-STRING2 = getenv("STRING_SESSION2", None)
+STRING1 = getenv("STRING_SESSION", "BQFQySMAA6R8Wg26CU5bWdv3qys7fzhClm9K0FtdI_NO7Ty9S6XOS6kX98vPhy779f_uUaVujfVTTybLT9cGGqpN62V6lNbIeIdQMYu8q9sKfp60La2Y6oEvMeo4A6YSa0Kem1TS9yxqLJYTjZB67AnVb7QvO-628zQ2DlscGBP6kc9c83pPV-D2amm6vjQvZlFB1wRzERf6NZMgq3VcxX66cHYkA2ZNoWN0O8wMYcAnqpqdWY-BlYXLwPx_135BAfwOQkhFauLw8h4kuHbHklOhk0dAbim0KQjQcuAya4cGJdw1qj_DzQtX_5c59If_Xe6ctTla7rXCpI0NF0TF9YYELvfAtgAAAAHK3lnhAA")
+STRING2 = getenv("STRING_SESSION2", "BQF4liQAMehVQRQG8dIX9mrXuzETLvNwAGc5qT7WZj2VJ5HKJ9TDi50BMvsTBMQHBNBBMvuf77Kpr28WeePbUmnjQ-L9NHXVSRo6O8JqQm4rYT-on4vTx0qHBkdM-mJZrAxd2MgGKqJgLNMEr7A6WfFxnIAalAU5N1hZIzoYqffPMi9EEvUSD_gJDHamF_MdnpE8dQasldBofuZX08LSRDbYNVOWUyPApSGX1sQ45X4tYw1YL8ONCs7tLUXYJ1s3VpwUUQgZjmsspqi-suq6Cj25pTRUBVQlqDNyZQ4kiU1-Gr3lpQ2oumtXA5ZQVeVYLHgzJ5KR8ojw2DpIUQaIm8vwUQGyUwAAAAGeHxvxAA")
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
