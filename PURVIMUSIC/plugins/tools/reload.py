8905e75d867588ad5559419c7b4d6019689eda85
𝐈𝛕ᷟ‌𝚣⃪ꙴ ⋆‌⃝๛𝐒𝚈ѕ𝐓𝚄M ٭》[🇮🇳] <ownersystum@gmail.com>
2024-11-26 14:29:39 +0530

    𓄂️𝆺𝅥⃝☠️ ‌⃪‌ ᷟ 𝐌‌ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏‌⃪🔥᪵᪳❛𝆺꯭𝅥༎ࠫ

--- PURVIMUSIC/plugins/tools/reload.py
+++ PURVIMUSIC/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(1052050212,7548798871)
+    & filters.user(1052050212)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
